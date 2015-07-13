f06d01d17610417bf6a0b733c239db1a2752f4fd
adamyarger <yargeradam@gmail.com>
2015-07-13 15:14:06 -0500

    feature passes with no label now

--- spec/support/features/post_helper.rb
+++ spec/support/features/post_helper.rb
@@ -1,7 +1,7 @@
 module Features
 	def create_post(post_image, post_content)
 		visit 'posts/new'
-		attach_file 'Image', post_image
+		attach_file 'post[image]', post_image
 		fill_in 'Write a description.', with: post_content
 		click_on 'Post'
 	end
