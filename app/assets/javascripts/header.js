24f107db817f0a03bb6c36cdc01e25aab2b2df0b
adamyarger <yargeradam@gmail.com>
2015-07-14 12:32:29 -0500

    right buttons working just need drop down

--- app/assets/javascripts/header.js
+++ app/assets/javascripts/header.js
@@ -2,7 +2,7 @@ $(document).ready(function($){
 	/********************************
 		open/close submenu on mobile
 	********************************/
-	$('.main-nav').on('click', function(event){
-		if($(event.target).is('.main-nav')) $(this).children('ul').toggleClass('is-visible');
+	$('.nav-bell').on('click', function(event){
+		if($(event.target).is('.nav-bell')) $(this).children('ul').toggleClass('is-visible');
 	});
 });
