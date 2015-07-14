e33d89919dc8467b2b803e3a6a475a199f463d91
adamyarger <yargeradam@gmail.com>
2015-07-14 16:06:25 -0500

    styled bottom

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ActiveRecord::Base
 	validates :user_id, :image, presence: true
 
 	validates_with AttachmentSizeValidator, :attributes => :image, :less_than => 2.megabytes
-	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, 
+	has_attached_file :image, :styles => { :medium => "1000x1000>", :thumb => "100x100>" }, 
 																				:default_url => "/images/blur_holder.jpg"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
