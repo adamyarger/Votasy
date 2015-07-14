6a307e1e2a18d3b7dfdf6452936aa0f92231b0d4
adamyarger <yargeradam@gmail.com>
2015-07-14 18:55:16 -0500

    adds flash messages styling

--- app/assets/javascripts/header.js
+++ app/assets/javascripts/header.js
@@ -1,5 +1,16 @@
+
+// Smal MQ drop down menu
 $(document).ready(function($){
 	$('div.nav-bell').on('click', function(event){
 		if($(event.target).is('.nav-bell')) $('.main-nav').children('ul').toggleClass('is-visible');
 	});
+});
+
+//flash message fade out
+$(document).ready(function(){
+  $(function() {
+   $('#flash').delay(500).slideDown('normal', function() {
+      $(this).delay(2500).slideUp();
+   });
+  });
 });
