eb626a613c4241937248145358fb988b04856ccf
adamyarger <yargeradam@gmail.com>
2015-07-15 15:01:11 -0500

    adds passing user show controller spec

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,11 +2,9 @@ require 'rails_helper'
 
 describe UsersController, type: :controller do
 	describe 'GET #show' do
-		
-
 		it 'renders the #show view' do
-			user = FactoryGirl.create :user
-			get :show, id: user
+			@user = FactoryGirl.create :user
+			get :show, id: @user
 			expect(response).to render_template :show
 		end
 	end
