a60a2ce9311ef2822d88b8529200fbcdcebbd9e4
adamyarger <yargeradam@gmail.com>
2015-07-15 12:09:19 -0500

    adds comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,24 @@
 class CommentsController < ApplicationController
+	before_filter :authenticate_user!
+
+	def create
+		@post = Post.find(params[:post_id])
+		@comment = Comment.create(comment_params)
+		@comment.user_id = current_user.id
+		@comment.post_id = @post.id
+
+		if @comment.save
+			flash[:success] = 'Comment Posted'
+			redirect_to post_path(@post)
+		else
+			flash[:error] = 'Something Went Wrong'
+			redirect_to post_path(@post)
+		end
+	end
+
+	private
+
+		def comment_params
+			params.require(:comment).permit(:body)
+		end
 end
--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -15,7 +15,26 @@ describe CommentsController, type: :controller do
 				post :create, { user_id: @user.id, post_id: @post, comment: @comment_attributes }
 				expect(Comment.count).to eq(1)
 			end
+
+			it 'renders the comment' do
+				post :create, { user_id: @user.id, post_id: @post, comment: @comment_attributes }
+				expect(response).to redirect_to @post
+			end
+		end
+
+		context 'with invalid attributes' do
+			before(:each) do
+				@user = FactoryGirl.create :user
+				@post = FactoryGirl.create :post
+				sign_in @user
+				@invalid_comment_attributes = { body: nil }
+			end
+
+			it 'does not create the post' do
+				post :create, { user_id: @user.id, post_id: @post, comment: @invalid_comment_attributes }
+				expect(Comment.count).to eq(0)
+			end
 		end
 	end
-
 end
+
