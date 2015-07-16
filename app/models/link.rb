834606a943ff2e76d95c22c7b4ba5fb817816611
adamyarger <yargeradam@gmail.com>
2015-07-15 20:29:11 -0500

    add embedly model method

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,3 +1,25 @@
 class Link < ActiveRecord::Base
 	belongs_to(:user)
+	validates :content, presence: true
+
+	after_create :update_from_embedly
+
+	def update_from_embedly
+    link = self
+    urls = [content]
+
+    embedly_api = Embedly::API.new(:key => Setting.embedly_key)
+    embedly_objs = embedly_api.oembed(:urls => urls, :maxwidth => 500, :width => 500, :autoplay => false)
+    embedly_obj = embedly_objs[0]
+    response_data = embedly_obj.marshal_dump
+    
+    link.title             =  response_data[:title]
+    link.author_url        =  response_data[:author_url]
+    link.provider_name     =  response_data[:provider_name]
+    link.html              =  response_data[:html]
+    link.thumbnail_url		 =	response_data[:thumbnail_url]
+    link.thumbnail_width   =  response_data[:thumbnail_width]
+    link.thumbnail_height  =  response_data[:thumbnail_height]
+    link.save
+  end
 end
--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -2,4 +2,5 @@ require 'rails_helper'
 
 describe Link do
 	it { should belong_to(:user) }
+	it { should validate_presence_of(:content) }
 end
