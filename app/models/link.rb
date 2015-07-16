f3c0616956dfe6cfea2ee241757a7edd0f9dd212
adamyarger <yargeradam@gmail.com>
2015-07-15 21:02:45 -0500

    embedly is working

--- app/models/link.rb
+++ app/models/link.rb
@@ -8,7 +8,7 @@ class Link < ActiveRecord::Base
     link = self
     urls = [content]
 
-    embedly_api = Embedly::API.new(:key => Setting.embedly_key)
+    embedly_api = Embedly::API.new(:key => "4e4f48f422454147b45661749719a0d3")
     embedly_objs = embedly_api.oembed(:urls => urls, :maxwidth => 500, :width => 500, :autoplay => false)
     embedly_obj = embedly_objs[0]
     response_data = embedly_obj.marshal_dump
