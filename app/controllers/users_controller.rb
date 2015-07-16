3a62f57f2fa3260d70f9e673ed40df3244dd2948
adamyarger <yargeradam@gmail.com>
2015-07-16 13:53:35 -0500

    adds index posts to user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,7 @@ class UsersController < ApplicationController
 	def show
 		@user = User.find(params[:id])
 		@posts = @user.posts
+		@links = Link.all
+		@array = (@posts.to_a + @links.to_a).sort_by(&:created_at)
 	end
 end
