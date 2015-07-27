3b284fcbc711d78e2e6b771b12c9a7b99844e20f
adamyarger <yargeradam@gmail.com>
2015-07-27 16:56:23 -0500

    passes faulty user show controller test

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,6 +2,11 @@ require 'rails_helper'
 
 describe UsersController, type: :controller do
 	describe 'GET #show' do
+		before(:each) do
+			@user = FactoryGirl.create :user
+			sign_in @user
+		end
+
 		it 'renders the #show view' do
 			@user = FactoryGirl.create :user
 			get :show, id: @user
