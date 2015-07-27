494c505a2370e1f38329d7ca1b3021858a8ff355
adamyarger <yargeradam@gmail.com>
2015-07-27 17:09:10 -0500

    adds following/follers controller tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -8,9 +8,33 @@ describe UsersController, type: :controller do
 		end
 
 		it 'renders the #show view' do
-			@user = FactoryGirl.create :user
+			# @user = FactoryGirl.create :user
 			get :show, id: @user
 			expect(response).to render_template :show
+		end
+	end
+
+	describe 'GET #following' do
+		before(:each) do
+			@user = FactoryGirl.create :user
+			sign_in @user
+		end
+
+		it 'renders the #following view' do
+			get :following, id: @user
+			expect(response).to render_template :show_follow
+		end
+	end
+
+	describe 'GET #followers' do
+		before(:each) do
+			@user = FactoryGirl.create :user
+			sign_in @user
+		end
+
+		it 'renders the #followers view' do
+			get :followers, id: @user
+			expect(response).to render_template :show_follow
 		end
 	end
 end
