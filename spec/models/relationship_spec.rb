ca3a39a92b44fcd85e4308457c1e53a2b8badc1f
adamyarger <yargeradam@gmail.com>
2015-07-27 12:59:13 -0500

    adds relationship model test

--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe Relationship, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Relationship do
+	before(:each) do
+		@follower = FactoryGirl.create(:user)
+		@followed = FactoryGirl.create(:user)
+
+		@relationship = Relationship.new(follower_id: @follower.id, followed_id: @followed.id)
+	end
+
+	it { should validate_presence_of(:follower_id) }
+	it { should validate_presence_of(:followed_id) }
+
+	it 'should create a new instance given vaild attributes' do
+		@relationship.save!
+	end
 end
