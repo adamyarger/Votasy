f4f4a8ac45bf1c7fb0cafb66fcaae3eff948bc9b
adamyarger <yargeradam@gmail.com>
2015-07-27 15:39:07 -0500

    adds conditional follow/unfollow button

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,13 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
   root 'posts#index'
 
-  resources :users, only: [:show]
+  resources :users, only: [:show] do
+  	member do
+  		get :following, :followers
+  	end
+  end
   resources :links
+  resources :relationships, only: [:create, :destroy]
   resources :posts, only: [:show, :create, :new] do
   	resources :comments
   end
