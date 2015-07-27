32b848973383f702f0de0373f4f6d5514c8db06f
adamyarger <yargeradam@gmail.com>
2015-07-27 18:49:24 -0500

    no destroy controller test for now

--- spec/controllers/relationships_controller_spec.rb
+++ spec/controllers/relationships_controller_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 describe RelationshipsController, type: :controller do
 	describe 'POST #create' do
+		
 		context 'with valid attributes' do
 			before(:each) do
 				@user = FactoryGirl.create :user
@@ -10,11 +11,10 @@ describe RelationshipsController, type: :controller do
 			end
 
 			it 'creates the relationship' do
-				post :create, { followed_id: @followed.id, following_id: @user.id }
+				post :create, { followed_id: @followed.id, follower_id: @user.id }
 				expect(Relationship.count).to eq(1)
 			end
 		end
-	end
 
-	
+	end
 end
