900fe8d9378c21a5a1200e8343165d85f187a169
adamyarger <yargeradam@gmail.com>
2015-07-27 15:56:39 -0500

    adds following and followers show actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,24 @@
 class UsersController < ApplicationController
+	before_action :authenticate_user!
+
 	def show
 		@user = User.find(params[:id])
 		@posts = @user.posts
 		@links = Link.all
 		@array = (@posts.to_a + @links.to_a).sort_by(&:created_at).paginate(:page => params[:page], :per_page => 20)
+	end
+
+	def following
+		@title = 'Following'
+		@user = User.find(params[:id])
+		@users = @user.following.paginate(:page => params[:page], :per_page => 20)
+		render 'show_follow'
+	end
+
+	def followers
+		@title = 'Followers'
+		@user = User.find(params[:id])
+		@users = @user.followers.paginate(:page => params[:page], :per_page => 20)
+		render 'show_follow'
 	end
 end
