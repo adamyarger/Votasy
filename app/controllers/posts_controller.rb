855f0986b9a0436c467ea4f92fa90c5aafbc3425
adamyarger <yargeradam@gmail.com>
2015-07-28 16:41:21 -0500

    acts as taggable is fully functional

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,6 +13,19 @@ class PostsController < ApplicationController
 		end
 	end
 
+	def index
+		if params[:tag]
+			@posts = Post.tagged_with(params[:tag])
+			@links = Link.tagged_with(params[:tag])
+			@array_set = (@posts.to_a + @links.to_a).sort_by(&:created_at)
+			@array = @array_set.paginate(:page => params[:page], :per_page => 10)
+		else
+			@posts = Post.all
+			@links = Link.all
+			@array = (@posts.to_a + @links.to_a).sort_by(&:created_at).paginate(:page => params[:page], :per_page => 10)
+		end
+	end
+
 	def new
 		@post = Post.new
 	end
