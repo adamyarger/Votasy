75deed3b8dec817c2a8f698a9ed58531d54eec4d
adamyarger <yargeradam@gmail.com>
2015-07-28 12:33:56 -0500

    fixes overflow problem

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,13 @@
 class StaticPagesController < ApplicationController
 	def feed
-		@posts = current_user.feed_posts
-		@links = current_user.feed_links
-		@array = (@posts.to_a + @links.to_a).sort_by(&:created_at).paginate(:page => params[:page], :per_page => 20)
+		if user_signed_in?
+			@posts = current_user.feed_posts
+			@links = current_user.feed_links
+			@array = (@posts.to_a + @links.to_a).sort_by(&:created_at).paginate(:page => params[:page], :per_page => 20)
+		else
+			@posts = Post.all
+			@links = Link.all
+			@array = (@posts.to_a + @links.to_a).sort_by(&:created_at).paginate(:page => params[:page], :per_page => 20)
+		end
 	end
 end
