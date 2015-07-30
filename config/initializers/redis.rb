5d662ffc8968b8ca629322758a35ff27820edb6b
adamyarger <yargeradam@gmail.com>
2015-07-29 19:47:37 -0500

    adds redis url directly

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -2,5 +2,5 @@
 # REDIS = Redis.new(:url => ENV['REDISTOGO_URL'])
 # ENV["REDIS_URL"] = ENV["REDISTOGO_URL"]
 
-uri = URI.parse(ENV["REDISTOGO_URL"] || "redis://localhost:6379/")
+uri = URI.parse(ENV['redis://redistogo:ef6a614bb7ad25506f16b8df0a1b56e8@catfish.redistogo.com:9809/'] || "redis://localhost:6379/")
 REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
